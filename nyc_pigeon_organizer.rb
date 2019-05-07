1175d88128af55ea3d5bb1d90bd8d2fa953dc660
Kevin Dai <daikevin19@yahoo.com>
2019-05-07 02:39:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(outer, inner), hash|
     inner.each do |trait_key, names|
       names.each do |name|
-        hash[name] ||= {}
+        hash[name] ||= {} # new hash puts name as key
         hash[name][outer] ||= []
         hash[name][outer] << trait_key.to_s
       end
